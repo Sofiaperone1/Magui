a970319d339175467a1a465e594fb4dd818336b5
Sofia Perone <sofiaperone62@gmail.com>
2024-08-05 22:31:08 -0300

    .

--- src/components/Carrousel/CarrouselShop.jsx
+++ src/components/Carrousel/CarrouselShop.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import CustomDotShop from "./CustomDotShop"
-import { CustomLeftArrow, CustomRightArrow} from "./CustomArrows"; // Importa tus flechas personalizadas
 import 'react-multi-carousel/lib/styles.css'; // Asegúrate de importar los estilos del carrusel
-import CI1 from "../../Imgs/carrouselInicio/CI1.jpg"
-import CI2 from "../../Imgs/carrouselInicio/CI2.jpg"
-import CI3 from "../../Imgs/carrouselInicio/CI3.jpg"
-import CI4 from "../../Imgs/carrouselInicio/CI4.jpg"
-import CI5 from "../../Imgs/carrouselInicio/CI5.jpg"
-import CI6 from "../../Imgs/carrouselInicio/CI6.jpg"
+
 
 
 // Definición del componente WithStyles, este es solo un ejemplo, asegúrate de ajustarlo a tu caso
--- src/components/Carrousel/CustomDotShop.jsx
+++ src/components/Carrousel/CustomDotShop.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import CI12 from "../../Imgs/carrouselInicio/CI12.jpg";
-import CI22 from "../../Imgs/carrouselInicio/CI22.jpg";
-import CI32 from "../../Imgs/carrouselInicio/CI32.jpg";
-import CI42 from "../../Imgs/carrouselInicio/CI42.jpg";
-import CI52 from "../../Imgs/carrouselInicio/CI52.jpg";
-import CI62 from "../../Imgs/carrouselInicio/CI62.jpg";
+
 
 // Estilo básico para los dots personalizados
 import './carousel-with-custom-dots.css'; // Asegúrate de tener este archivo CSS
